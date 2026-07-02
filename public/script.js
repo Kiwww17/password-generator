c301bf33bf90c41f70d33a97a601b9371dccc68c
Kiwww17 <wimarizky17@gmail.com>
2026-07-02 14:22:07 +0700

    another change

--- public/script.js
+++ public/script.js
@@ -4,6 +4,7 @@ const generate = document.getElementById('generateBtn');
 const copy = document.getElementById('copy');
 const display = document.getElementById('display');
 const regenerateBtn = document.getElementById('regenerateBtn');
+const notification = document.getElementById('notification');
 
 
 inputRange.addEventListener('input', () => {
@@ -14,11 +15,11 @@ inputInp.addEventListener('input', () => {
     inputRange.value = inputInp.value;
 });
 
+let password = []
 generate.addEventListener('click', function(){
     inputRange.value;
     const template = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890#${}.@_'
     
-    let password = []
     for(let i = 0; i < inputRange.value; i++){
         const selector  = Math.floor((Math.random()*69)-0);
         password.push(template.charAt(selector))
@@ -32,6 +33,13 @@ generate.addEventListener('click', function(){
 copy.addEventListener('click', function(){
     copy.textContent = '✅'
     navigator.clipboard.writeText(password)
+    notification.style.visibility = 'visible'
+    notification.style.transition = 'all 300ms'
+    
+    setTimeout(() => {
+        notification.style.visibility = 'hidden'
+    }, 2000)
+
 
 })
 
